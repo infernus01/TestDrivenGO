ecd24efc39760e93c3631739ff5bd62b945a9865
infernus01 <sbmbhardwaj01@gmail.com>
2025-03-16 22:38:20 +0530

    Update: Multiple test cases and Helper function

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	t.Run("saying hello", func(t *testing.T) {
+		got := Hello("infernus")
+		want := "Hello, infernus"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("if empty string, default it to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // the error points to the actual test case, and with commenting this out, the error would point to the helper function.
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
