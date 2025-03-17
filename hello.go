484aef1c17d4701413a5f06b8c470496284a7cba
infernus01 <sbmbhardwaj01@gmail.com>
2025-03-17 14:04:45 +0530

    Update Hello function with language parameter

--- hello.go
+++ hello.go
@@ -1,15 +1,35 @@
 package main
 
-import "fmt"
 
-const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
 }
-func main() {
-	fmt.Println(Hello("infernus"))
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
+// func main() {
+// 	fmt.Println(Hello("infernus"))
+// }
