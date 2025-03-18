7001725deecd5abecf63f4d6b8469636b3791bb8
infernus01 <sbmbhardwaj01@gmail.com>
2025-03-18 23:08:43 +0530

    Update: Add func for sumAllTails

--- ArraysnSlices/DeepEqual.go
+++ ArraysnSlices/DeepEqual.go
@@ -1,16 +1,24 @@
 package ArraysnSlices
 
-func SumAll(numbersToSum ...[]int) []int {
-	// lenOfNumbers := len(numbersToSum)
-	// sums := make([]int, lenOfNumbers)
+// func SumAll(numbersToSum ...[]int) []int {
+// 	// lenOfNumbers := len(numbersToSum)
+// 	// sums := make([]int, lenOfNumbers)
 
-	// for i, numbers := range numbersToSum {
-	// 	sums[i] = Sum(numbers)
-	// }
+// 	// for i, numbers := range numbersToSum {
+// 	// 	sums[i] = Sum(numbers)
+// 	// }
+
+// 	// var sums []int
+// 	// for _, numbers := range numbersToSum {
+// 	// 	sums = append(sums, Sum(numbers)) //append function takes a slice and a new value, then returns a new slice with all the items in it. 
+// 	// }
+// 	// return sums
+// }
+
+func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
-	for _, numbers := range numbersToSum {
-		sums = append(sums, Sum(numbers)) //append function takes a slice and a new value, then returns a new slice with all the items in it. 
+	for _,numbers:=range numbersToSum {
+		sums=append(sums,Sum(numbers[1:]))
 	}
 	return sums
 }
-
