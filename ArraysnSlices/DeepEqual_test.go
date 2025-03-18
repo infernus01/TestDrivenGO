ea5e563a33db1065db0c6adf67f247a57d513959
infernus01 <sbmbhardwaj01@gmail.com>
2025-03-18 23:08:23 +0530

    Update: Add test case for sumAllTails

--- ArraysnSlices/DeepEqual_test.go
+++ ArraysnSlices/DeepEqual_test.go
@@ -5,11 +5,22 @@ import (
 	"testing"
 )
 
-func TestSumAll(t *testing.T) {
-	got := SumAll([]int{1, 2}, []int{0, 9})
-	want := []int{3, 9}
+// func TestSumAll(t *testing.T) {
+// 	got := SumAll([]int{1, 2}, []int{0, 9})
+// 	want := []int{3, 9}
 
-	if !reflect.DeepEqual(got, want) { //!= will not work here as we are comparing slices, it will throw error
-		t.Errorf("got %v want %v", got, want)
-	}
+// 	if !reflect.DeepEqual(got, want) { //!= will not work here as we are comparing slices, it will throw error
+// 		t.Errorf("got %v want %v", got, want)
+// 	}
+// }
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("sum the tails of slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
