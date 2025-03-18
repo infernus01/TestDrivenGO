fa93158d1a55dddfc06e6c60890c8e93902a8265
infernus01 <sbmbhardwaj01@gmail.com>
2025-03-18 22:26:00 +0530

    Update sumAll function: use append function

--- ArraysnSlices/DeepEqual.go
+++ ArraysnSlices/DeepEqual.go
@@ -1,11 +1,15 @@
 package ArraysnSlices
 
 func SumAll(numbersToSum ...[]int) []int {
-	lenOfNumbers := len(numbersToSum)
-	sums := make([]int, lenOfNumbers)
+	// lenOfNumbers := len(numbersToSum)
+	// sums := make([]int, lenOfNumbers)
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers)) //append function takes a slice and a new value, then returns a new slice with all the items in it. 
 	}
 	return sums
 }
